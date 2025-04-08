fe92aea312e0cea021a519ebe745a31487ff507b
SahalFajri <sahalfajri879@gmail.com>
2025-04-08 15:22:45 +0700

    Create project using Laravel & TailwindCSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SwapHub</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <h1 class="text-sm">Anggota</h1>
+  <ul>
+    <li>Salik</li>
+    <li>Khalid</li>
+    <li>Daffa</li>
+    <li>Viriya</li>
+    <li>Davin</li>
+    <li>Sahal</li>
+  </ul>
+</body>
+
+</html>
