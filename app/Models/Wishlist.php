9097ced11863ed1b80cc41acdb5b3affe6504d80
SahalFajri <sahalfajri879@gmail.com>
2025-04-09 10:27:44 +0700

    SP-35 Create wishlist migration and model

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Wishlist extends Model
+{
+    protected $table = 'wishlist';
+
+    protected $fillable = [
+        "id_user",
+        "id_barang"
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function barang(): BelongsTo
+    {
+        return $this->belongsTo(Barang::class);
+    }
+}
