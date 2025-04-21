97be58bd4162d632ba8949942f842a48801f2f33
Benedict Davin <b.davin.p@gmail.com>
2025-04-21 16:04:07 +0700

    SP-51 Mengubah model History

--- app/Models/History.php
+++ app/Models/History.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class History extends Model
 {
-    //
+    protected $table = 'history';
+    protected $primaryKey = 'id_history';
+
+    protected $fillable = [
+        'id_penukaran_barang',
+    ];
+
+    public function penukaran_barang(): BelongsTo
+    {
+        return $this->belongsTo(Penukaran::class, 'id_penukaran_barang');
+    }
 }
