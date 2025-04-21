cfeae573aaa7667c1e52006c9cf113b5ce5c1b50
Benedict Davin <b.davin.p@gmail.com>
2025-04-21 16:06:52 +0700

    SP-53 Mengubah route untuk history

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/history.index', function () {
+//     return view('index');
+// });
+
+use App\Http\Controllers\HistoryController;
+
+Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+Route::get('/history/{id}', [HistoryController::class, 'show'])->name('history.show');
+
